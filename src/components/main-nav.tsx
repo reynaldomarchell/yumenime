67ba0aba195d7e17fca09643813e2e54c30f9133
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-05-20 00:51:03 +0700

    chore: fix navbar padding

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -4,20 +4,18 @@ import Search from "./search";
 
 export default function MainNav() {
   return (
-    <>
-      <nav className="flex items-center justify-between border-b pb-5">
-        <Link href="/">
-          <Image
-            src="/yumenime-logo.png"
-            width={70}
-            height={23}
-            alt="logo"
-            className="w-20 md:w-24"
-          />
-        </Link>
+    <nav className="flex items-center justify-between border-b pb-2 md:pb-5">
+      <Link href="/">
+        <Image
+          src="/yumenime-logo.png"
+          width={70}
+          height={23}
+          alt="logo"
+          className="w-20 md:w-24"
+        />
+      </Link>
 
-        <Search />
-      </nav>
-    </>
+      <Search />
+    </nav>
   );
 }
