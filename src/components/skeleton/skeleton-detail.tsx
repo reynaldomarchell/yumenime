ffe8337d59524fbbcafd81c260c04bfafb97c9ab
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-05-21 17:23:12 +0700

    style(skeleton): change card width

--- src/components/skeleton/skeleton-detail.tsx
+++ src/components/skeleton/skeleton-detail.tsx
@@ -2,43 +2,41 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function SkeletonDetail() {
   return (
-    <div>
-      <div className="my-5 flex max-h-fit w-full flex-col space-y-2 pb-1 md:space-y-3">
-        <Skeleton className="mb-2 aspect-[3/1] w-full rounded-xl object-cover" />
-        <div className=" flex flex-row justify-between">
-          <div className="w-2/4 space-y-4">
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-          </div>
-          <div className="mb-2 w-1/4 space-y-4">
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-          </div>
-        </div>
-        <div className="mb-2 w-full space-y-2">
+    <div className="my-5 flex h-full w-full flex-col gap-2 pb-1 md:gap-3">
+      <Skeleton className="mb-2 aspect-[3/1] w-full rounded-xl object-cover" />
+      <div className=" flex flex-row justify-between">
+        <div className="w-2/4 space-y-4">
           <Skeleton className="h-4 w-full" />
           <Skeleton className="h-4 w-full" />
           <Skeleton className="h-4 w-full" />
           <Skeleton className="h-4 w-full" />
           <Skeleton className="h-4 w-full" />
         </div>
-        <div className="flex w-full gap-4 overflow-hidden">
-          {Array.from({ length: 30 }).map((_, index) => (
-            <div className="flex flex-col space-y-3" key={index}>
-              <Skeleton className="aspect-[2/3] max-w-24 rounded-xl" />
-              <div className="space-y-2">
-                <Skeleton className="h-4 w-24" />
-                <Skeleton className="h-4 w-24" />
-              </div>
+        <div className="mb-2 w-1/4 space-y-4">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+      </div>
+      <div className="mb-2 w-full space-y-2">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+      </div>
+      <div className="flex w-full gap-4 overflow-hidden">
+        {Array.from({ length: 30 }).map((_, index) => (
+          <div className="flex flex-col space-y-3" key={index}>
+            <Skeleton className="aspect-[2/3] max-w-28 rounded-xl md:max-w-40" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-28 md:w-40" />
+              <Skeleton className="h-4 w-28 md:w-40" />
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/skeleton/skeleton-slider.tsx
+++ src/components/skeleton/skeleton-slider.tsx
@@ -5,10 +5,10 @@ export function SkeletonSlider() {
     <div className="my-3 flex w-full gap-4 overflow-hidden py-4">
       {Array.from({ length: 30 }).map((_, index) => (
         <div className="flex flex-col space-y-3" key={index}>
-          <Skeleton className="aspect-[2/3] max-w-24 rounded-xl" />
+          <Skeleton className="aspect-[2/3] max-w-28 rounded-xl md:max-w-40" />
           <div className="space-y-2">
-            <Skeleton className="h-4 w-24" />
-            <Skeleton className="h-4 w-24" />
+            <Skeleton className="h-4 w-28 md:w-40" />
+            <Skeleton className="h-4 w-28 md:w-40" />
           </div>
         </div>
       ))}
