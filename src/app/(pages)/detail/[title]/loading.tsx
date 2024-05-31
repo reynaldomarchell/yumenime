bbc987c0fe7a58812bcc3a5ef9e7fce67c139d93
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-05-31 14:42:27 +0700

    style(detail): change padding on load

--- src/app/(pages)/detail/[title]/loading.tsx
+++ src/app/(pages)/detail/[title]/loading.tsx
@@ -1,5 +1,9 @@
 import SkeletonDetail from "@/components/skeleton/skeleton-detail";
 
 export default function Loading() {
-  return <SkeletonDetail />;
+  return (
+    <div className="flex h-full w-full flex-col px-8">
+      <SkeletonDetail />
+    </div>
+  );
 }
